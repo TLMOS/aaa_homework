e222c414200adacf609edf39da6414b4f8d1f01f
Artem Pokroy <pokroy-tema@yandex.ru>
2022-10-26 20:45:46 +0300

    hw_3 fix

--- python/lesson_3/advert.py
+++ python/lesson_3/advert.py
@@ -15,21 +15,19 @@ class FromMapping:
     @staticmethod
     def __is_correct_mapping(mapping: Mapping) -> bool:
         """Check if all mapping keys are strings"""
-        return all(map(lambda x: type(x) is str, mapping.keys()))
+        return all(map(lambda x: isinstance(x, str), mapping.keys()))
 
     def __init__(self, mapping: Mapping[str, Any]):
         for key, value in mapping.items():
             if iskeyword(key):
                 key += '_'
             if isinstance(value, Mapping) and self.__is_correct_mapping(value):
-                self.__dict__[key] = FromMapping(value)
-            elif isinstance(value, Sequence):
+                value = FromMapping(value)
+            if isinstance(value, Sequence):
                 for i, x in enumerate(value):
-                    if type(x) is Mapping and self.__is_correct_mapping(x):
+                    if isinstance(x, Mapping) and self.__is_correct_mapping(x):
                         value[i] = FromMapping(x)
-                self.__dict__[key] = value
-            else:
-                self.__dict__[key] = value
+            setattr(self, key, value)
 
 
 class Advert(ColorizeMixin):
@@ -38,14 +36,9 @@ class Advert(ColorizeMixin):
     repr_color_code = 32  # Default color code (Green)
 
     def __init__(self, mapping: Mapping[str, Any]):
-        if '__price' in mapping:
-            del mapping['__price']
-        if 'price' in mapping:
-            self.price = mapping['price']
-            del mapping['price']
-        else:
-            self.price = 0
-        self.__dict__.update(**FromMapping(mapping).__dict__)
+        self.__price = 0
+        for key, value in vars(FromMapping(mapping)).items():
+            setattr(self, key, value)
 
     def __repr__(self):
         return f'{self.title} | {self.price} ₽'
